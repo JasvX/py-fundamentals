4a5148f26aa16e0da73647f253c684133a9655c0
root <root@localhost>
2022-05-23 01:29:05 -0500

    auto publish 1653287344496

--- src/01_list.py
+++ src/01_list.py
@@ -3,8 +3,25 @@
 # Crear una lista 
 # name_list = ["valores", "separados", "por", "comas"]
 
-list_de_str = ["uno", "dos", "tres"]
-print(list_de_str)
+list_str = ["uno", "dos", "tres"]
+print(list_str)
 
-list_de_n = [1, 2, 3, 4]
-print(list_de_n)
+list_n = [1, 2, 3, 4]
+print(list_n)
+
+# acceder a los valores de una lista mediante su index
+# el indice comienza en 0 desde el primer elemento de la lista
+
+print(list_str[0] , "es el primer elemento de list_str ")
+
+print(list_n[1], "es el segundo elemento de list_n")
+
+# para conocer el indice de un elemento se usa el método index()
+
+print(list_n.index(4), "es el indice del elemento 4")
+
+# se pueden usar asignaciones como en cualquier variable
+list_n[3] = 5
+print("list_n[3] = 5 \nahora el inice 3 de list_n es: \n", list_n[3])
+
+

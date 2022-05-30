be8c3a73c4e3876aea81ce6bcfa3f7e7d635839e
root <root@localhost>
2022-05-30 06:35:54 -0500

    auto publish 1653910553752

--- src/02_loops.py
+++ src/02_loops.py
@@ -1,1 +1,26 @@
 
+my_list = [0,1,2,3,4,5,6,7,8,9]
+
+# for loop
+for e in my_list:
+  print(e)
+  
+print()
+  
+# break, continue
+for e in my_list:
+  if e > 4:
+    break
+  print(e)
+  
+print()
+
+for e in my_list:
+  if e == 4:
+    continue
+  print(e)
+  
+
+  
+
+
